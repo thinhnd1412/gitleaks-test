b1be6d6173250e4298a1eafeb8049d9c4287ef99
Thinh Nguyen <thinhnd1412@gmail.com>
2023-03-23 18:50:59 +0700

    update insecure text

--- src/main/java/com/houarizegai/calculator/Test.java
+++ src/main/java/com/houarizegai/calculator/Test.java
@@ -1,24 +1,40 @@
 public class Test {
     public static void main(String[] args) {
-        String awsManagerId = "AKIA12345678901234567";
-        String awsSecretKey = "aws_secret_key_12345678901234567890123456789012345678";
-        String awsMwsKey = "amzn.mws.12345678-1234-1234-1234-123456789012";
-        String facebookSecretKey = "fb_secret_key_12345678901234567890123456789012";
-        String facebookClientId = "fb_client_id_123456789012345";
-        String twitterSecretKey = "twitter_secret_key_1234567890123456789012345678901234567";
-        String twitterClientId = "twitter_client_id_1234567890123456789";
-        String githubKey = "github_key_123456789012345678901234567890123456";
-        String linkedInClientId = "linkedin_client_id_123456789012";
-        String linkedInSecretKey = "linkedin_secret_key_1234567890123456";
-        String slackKey = "xoxb-123456789012-123456789012-1234567890123456789012345678901234";
-        String asymmetricPrivateKey = "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCI3JHQRmttKjDd\nIjWJFyLrf15TtOaETP9EiWjKz1phfOPRPHm2wAA/1FqD3jWOyHmKHnvcfw8ltvC+\n...\n-----END PRIVATE KEY-----";
-        String genericCredential = "api_key_123456789012345678901234567890123456789012345";
-        String genericConnectionString = "redis://username:password@localhost:6379/0";
-        String googleApiKey = "AIzaSyD-7xGjXXKm7MqfpqF9BZsU6C1NrwKcsYs";
-        String herokuApiKey = "heroku_api_key_12345678901234567890123456789012345678";
-        String mailChimpApiKey = "mailchimp_api_key_12345678901234567890123456789012-us1";
-        String mailgunApiKey = "mailgun_api_key_12345678901234567890123456789012";
-        String payPalBraintreeAccessToken = "access_token$production$1234567890123456$12345678901234567890123456789012";
-        String picaticApiKey = "sk_live_1234567890123456789012345678901234567890";
+        // AWS Manager ID
+        private static final String AWS_MANAGER_ID = "AKIAIOSFODNN7EXAMPLE";
+        
+        // AWS Secret Key
+        private static final String AWS_SECRET_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+        
+        // AWS MWS key
+        private static final String AWS_MWS_KEY = "amzn.mws.00000000-0000-0000-0000-000000000000";
+        
+        // Facebook Secret Key
+        private static final String FB_SECRET_KEY = "0123456789abcdef0123456789abcdef";
+        
+        // Facebook Client ID
+        private static final String FB_CLIENT_ID = "123456789012345";
+        
+        // Twitter Secret Key
+        private static final String TWITTER_SECRET_KEY = "0123456789abcdefghijklmnopqrstuvwxyz0123";
+        
+        // Twitter Client ID
+        private static final String TWITTER_CLIENT_ID = "abcdefghijklopmn";
+        
+        // Github API key
+        private static final String GITHUB_API_KEY = "0123456789012345678901234567890123456789";
+        
+        // LinkedIn Client ID
+        private static final String LINKEDIN_CLIENT_ID = "123456789012";
+        
+        // LinkedIn Secret Key
+        private static final String LINKEDIN_SECRET_KEY = "abcdefghijklopmn";
+        
+        // Slack webhook
+        private static final String SLACK_WEBHOOK = "https://hooks.slack.com/services/T12345678/B12345678/Abcdefghijklmnopqrstuvwxyz0123456";
+        
+        // Asymmetric Private Key
+        private static final String ASYMMETRIC_PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCKptZ+CHU6H/sU\ns3dvmwLs2y17x/aRPKvEsGImJiR53hruZTjTzzTf8fTRzHsbQ2sKcOq3gJ8nbk45\nTSLT9O9RdMsnmyFLrTFxz0GzrTTA0fDlUly1CwiZk6UZ/l6X9dkE2FXeBxudj5S7\n92o5wTC2QqdMpbm4nM4Mfij9pxmLzhafyjW1yv7R+RIMwDtlIaZtWzNtQkZb9q3t\np8WbvnfOwu+Rkpp42w7PTj8WtOloDQIY9t3q3tF8zS5jDv5P5LkUp5QnzJGZB0jV\nbk9itf+7VcaLxU+PbUJdLCmIV1fjZ1MBU6HqbU6Y9XbnxEb2L21+tzNl72ckz/MN\n3qJ3PM+NAgMBAAECggEBAJXnJlNLPW6zrU0r0U6G+ouBywNhUZJ0U6OK9c"
+
     }
 }
